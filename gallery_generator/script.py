41cf5029e843ddb182ef7a6522c1c477a8717a7c
Pierre Beaujean <pierreb24@gmail.com>
2022-11-14 20:00:44 +0100

    get tags in the correct order

--- gallery_generator/script.py
+++ gallery_generator/script.py
@@ -3,11 +3,12 @@ import pathlib
 import sys
 from datetime import datetime
 from jinja2 import Environment, select_autoescape, FileSystemLoader
+from sqlalchemy import select, func
 
 from gallery_generator import logger
 
 from gallery_generator.files import create_config_dirs, CONFIG_DIR_NAME
-from gallery_generator.database import GalleryDatabase, Picture, Category, Tag
+from gallery_generator.database import GalleryDatabase, Picture, Category, Tag, tag_picture_at
 from gallery_generator.files import seek_pictures
 from gallery_generator.tag import TagManager
 from gallery_generator.picture import create_picture_object
@@ -135,8 +136,6 @@ def command_update(root: pathlib.Path, db: GalleryDatabase, target: pathlib.Path
     template_index = env.get_template('index.html')
     template_tag = env.get_template('tag.html')
 
-    now = datetime.now().strftime('%d/%m/%Y')
-
     # TODO: CSS
 
     # create thumb types
@@ -149,13 +148,46 @@ def command_update(root: pathlib.Path, db: GalleryDatabase, target: pathlib.Path
         # create thumbnailer
         thumbnailer = Thumbnailer(root, target, session, thumb_types)
 
+        # common
+        common_kwargs = {'thumbnailer': thumbnailer, 'now': datetime.now().strftime('%d/%m/%Y')}
+        common_kwargs.update(**CONFIG['page_content'])
+
         # create thumbnail directory
         path_thumbnail = target / thumbnailer.THUMBNAIL_DIRECTORY
         if not path_thumbnail.exists():
             path_thumbnail.mkdir()
 
-        # go through all categories
-        categories = session.scalars(Category.select()).all()
+        # fetch categories & tags
+        categories = session.scalars(Category.select().order_by(Category.id)).all()
+        tags_per_cat_dic = {}
+        thumbnails_dic = {}
+        categories_dic = {}
+
+        for category in categories:
+            subq = select(
+                tag_picture_at.c.left_id,
+                tag_picture_at.c.right_id,
+                func.max(Picture.exif_datetime_original)
+            )\
+                .join(Picture, tag_picture_at.c.right_id == Picture.id)\
+                .group_by(tag_picture_at.c.left_id).subquery()
+
+            q = select(Tag, Picture)\
+                .where(Tag.category_id == category.id)\
+                .join(subq, subq.c.left_id == Tag.id)\
+                .join(Picture, subq.c.right_id == Picture.id)\
+                .order_by(Picture.exif_datetime_original.desc())
+
+            categories_dic[category.slug] = category
+            tag_and_thumb = session.execute(q).all()
+            tags_per_cat_dic[category.slug] = [c[0] for c in tag_and_thumb]
+            thumbnails_dic.update(**dict((t.slug, p) for t, p in tag_and_thumb))
+
+        common_kwargs['categories'] = categories_dic
+        common_kwargs['tags_per_cat'] = tags_per_cat_dic
+        common_kwargs['thumbnails'] = thumbnails_dic
+
+        # generate pages for category and tags
         for category in categories:
 
             # create directory for category
@@ -163,29 +195,39 @@ def command_update(root: pathlib.Path, db: GalleryDatabase, target: pathlib.Path
             if not path_category.exists():
                 path_category.mkdir()
 
-            for tag in category.tags:
+            for tag in tags_per_cat_dic[category.slug]:
                 logger.info('GENERATE {}'.format(tag.get_url()))
+
                 # update tag
                 tag.update_from_file(root / CONFIG_DIR_NAME / TagManager.TAG_DIRECTORY)
+
+                # get pictures
+                pictures = session.scalars(
+                    Picture.select()
+                    .join(tag_picture_at, tag_picture_at.c.right_id == Picture.id)
+                    .join(Tag, Tag.id == tag_picture_at.c.left_id)
+                    .where(Tag.id == tag.id)
+                    .order_by(Picture.exif_datetime_original)
+                ).all()
 
                 # render
                 path_tag = target / tag.get_url()
                 with path_tag.open('w') as f:
                     f.write(template_tag.render(
-                        thumbnailer=thumbnailer,
-                        now=now,
-                        **CONFIG['page_content'],
-                        tag=tag
+                        **common_kwargs,
+                        tag=tag,
+                        pictures=pictures
                     ))
 
+        # generate index
         logger.info('GENERATE index.html')
         with (target / 'index.html').open('w') as f:
             f.write(template_index.render(
-                thumbnailer=thumbnailer,
-                now=now,
-                **CONFIG['page_content'],
-                categories=categories
+                **common_kwargs,
+                categories_to_show=('album', 'date')
             ))
+
+        # TODO: additional pages
 
 
 def main():
